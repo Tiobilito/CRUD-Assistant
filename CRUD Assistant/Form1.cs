3644ee13d0f50df7cbea3373b881265d922c4643
André Josué Marín González <andre.marin1135@alumnos.udg.mx>
2025-02-09 19:07:35 -0600

    Ventana Para añadir registros añadida (base)

--- CRUD Assistant/Form1.cs
+++ CRUD Assistant/Form1.cs
@@ -16,5 +16,11 @@ namespace CRUD_Assistant
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
     }
 }
---
+++ CRUD Assistant/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Assistant
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    FormAcademico.Visible = true;
+                    FormAlumnos.Visible = false;
+                    FormGrupos.Visible = false;
+                    break;
+                case 1:
+                    FormAcademico.Visible = false;
+                    FormAlumnos.Visible = false;
+                    FormGrupos.Visible = true;
+                    break;
+                case 2:
+                    FormAcademico.Visible = false;
+                    FormAlumnos.Visible = true;
+                    FormGrupos.Visible = false;
+                    break;
+                default:
+                    FormAcademico.Visible = false;
+                    FormAlumnos.Visible = false;
+                    FormGrupos.Visible = false;
+                    break;
+            }
+        }
+    }
+}
